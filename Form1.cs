8696d5043b5f03a7b0c74637779b1e5f3bf2a3a5
Mateusz Kurowski <kurowskimateusz04@gmail.com>
2023-10-25 02:57:15 +0200

    Remove unused usings

--- Form1.cs
+++ Form1.cs
@@ -1,17 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-
 //Dodajemy biblioteki
 using System.IO;    // Osbługa plików tekstowych
 using System.Net;   // Obsługa połączenia do serwera FTP
+using System.Text;
 using System.Threading;
+using System.Windows.Forms;
 
 namespace FakeMinecraft
 {
